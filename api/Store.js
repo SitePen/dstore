583d9a1a80aaaf6937350aed04f7c6566f03e18a
Kris Zyp <kriszyp@gmail.com>
2013-11-11 15:51:54 -0700

    API Updates

--- api/Store.js
+++ api/Store.js
@@ -3,7 +3,131 @@ define(["../../_base/declare"], function(declare){
 // module:
 //		dojo/api/Store
 
-var Store = declare(null, {
+var Collection = declare(null, {
+	// summary:
+	//		This is an abstract API that for a collection of items, which can be filtered,
+	//		sorted, and sliced to create new collections
+	//		Every method and property is optional, and is only needed if the functionality
+	//		it provides is required.
+	//		Every method may return a promise for the specified return value if the
+	//		execution of the operation is asynchronous (except
+	//		for query() which already defines an async return value).
+	//		Note that the objects in the collection may not be immediately retrieved from
+	//		the underlying data storage until they are actually accessed through forEach() or then().
+
+	filter: function(query){
+		// summary:
+		//		Filters the collection, returning a new subset collection
+		// query: String|Object|Function
+		//		The query to use for retrieving objects from the store.
+		// returns: Collection
+	},
+	sort: function(property, descending) {
+		// summary:
+		//		Sorts the collection, returning a new collection with the objects sorted
+		// property: String|Function
+		//		The property to sort on. Alternately a function can be provided to sort with
+		// descending?: Boolean
+		//		Indicate if the sort order should be descending (defaults to ascending)
+		// returns: Collection
+	},
+	range: function(start, end) {
+		// summary:
+		//		Retrieves a range of objects from the collection, returning a new collection with the objects indicated by the range
+		// start: Number
+		//		The starting index of objects to return (0-indexed)
+		// end?: Number
+		//		The exclusive end of objects to return
+		// returns: Collection
+	},
+	forEach: function(callback, thisObject){
+		// summary:
+		//		Iterates over the query results, based on
+		//		https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/forEach.
+		//		Note that this may executed asynchronously. The callback may be called
+		//		after this function returns.
+		// callback:
+		//		Function that is called for each object in the query results
+		// thisObject:
+		//		The object to use as |this| in the callback.
+	},
+	map: function(callback, thisObject){
+		// summary:
+		//		Maps the query results, based on
+		//		https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/map.
+		//		Note that this may executed asynchronously. The callback may be called
+		//		after this function returns.
+		// callback:
+		//		Function that is called for each object in the query results
+		// thisObject:
+		//		The object to use as |this| in the callback.
+		// returns: dojo/store/api/Store.Collection
+	},
+	then: function(callback, errorHandler){
+		// summary:
+		//		This registers a callback for when the query is complete, if the query is asynchronous.
+		//		This is an optional method, and may not be present for synchronous queries.
+		// callback:
+		//		This is called when the query is completed successfully, and is passed a single argument
+		//		that is an array representing the query results.
+		// errorHandler:
+		//		This is called if the query failed, and is passed a single argument that is the error
+		//		for the failure.
+	},
+	observe: function(listener, excludeInPlaceUpdates){
+		// summary:
+		//		This registers a callback for notification of when data is modified in the query results.
+		//		This is an optional method, and is usually provided by dojo/store/Observable.
+		// listener: Function
+		//		The listener function is called when objects in the query results are modified
+		//		to affect the query result. The listener function is called with the following arguments:
+		//		| listener(object, removedFrom, insertedInto);
+		//
+		//		- The object parameter indicates the object that was create, modified, or deleted.
+		//		- The removedFrom parameter indicates the index in the result array where
+		//		the object used to be. If the value is -1, then the object is an addition to
+		//		this result set (due to a new object being created, or changed such that it
+		//		is a part of the result set).
+		//		- The insertedInto parameter indicates the index in the result array where
+		//		the object should be now. If the value is -1, then the object is a removal
+		//		from this result set (due to an object being deleted, or changed such that it
+		//		is not a part of the result set).
+		// excludeInPlaceUpdates:
+		//		This indicates whether or not to include object updates that do not affect
+		//		the inclusion or order of the object in the query results. By default this is false,
+		//		but if set to true it means means that if any object is updated in such a way that it remains
+		//		in the result set and it's position in result sets is not affected, then the listener
+		//		will not be fired.
+		//	returns: Collection
+
+	},
+	// model: Function
+	//		This constructor represents the data model class to use for the objects returned
+	//		from the store. All objects returned from the store should have their [[proto]] set
+	//		to the prototype property of the model, such that objects from this store should
+	//		be return true from `object instanceof store.model`.
+	model: Function,
+	
+	// total: Number|Promise?
+	//		This property should be included in if the query options included the "count"
+	//		property limiting the result set. This property indicates the total number of objects
+	//		matching the query (as if "start" and "count" weren't present). This may be
+	//		a promise if the query is asynchronous.
+	total: 0,
+	// sorted: Object
+	//		If the collection has been sorted, this is an object that indicates the property that it was
+	//		sorted on and if it was descending
+	sorted: {},
+	// filtered: String|Object|Function|Array
+	//		If the collection has been filtered, this is an object that indicates the query that
+	//		was used to filter it.
+	filtered: {},
+	// ranged: Object
+	//		If the collection has been subsetted with range, this is an object that indicates the start
+	//		and end of the range
+	ranged: {}
+});
+var Store = declare(Collection, {
 	// summary:
 	//		This is an abstract API that data provider implementations conform to.
 	//		This file defines methods signatures and intentionally leaves all the
@@ -82,26 +206,6 @@ var Store = declare(null, {
 			}
 		}
 	},
-	query: function(query, options){
-		// summary:
-		//		Queries the store for objects. This does not alter the store, but returns a
-		//		set of data from the store.
-		// query: String|Object|Function
-		//		The query to use for retrieving objects from the store.
-		// options: dojo/store/api/Store.QueryOptions
-		//		The optional arguments to apply to the resultset.
-		// returns: dojo/store/api/Store.QueryResults
-		//		The results of the query, extended with iterative methods.
-		//
-		// example:
-		//		Given the following store:
-		//
-		//	...find all items where "prime" is true:
-		//
-		//	|	store.query({ prime: true }).forEach(function(object){
-		//	|		// handle each object
-		//	|	});
-	},
 	transaction: function(){
 		// summary:
 		//		Starts a new transaction.
@@ -111,14 +215,12 @@ var Store = declare(null, {
 		// returns: dojo/store/api/Store.Transaction
 		//		This represents the new current transaction.
 	},
-	getChildren: function(parent, options){
+	getChildren: function(parent){
 		// summary:
 		//		Retrieves the children of an object.
 		// parent: Object
 		//		The object to find the children of.
-		// options: dojo/store/api/Store.QueryOptions?
-		//		Additional options to apply to the retrieval of the children.
-		// returns: dojo/store/api/Store.QueryResults
+		// returns: dojo/store/api/Store.Collection
 		//		A result set of the children of the parent object.
 	},
 	getMetadata: function(object){
@@ -132,6 +234,7 @@ var Store = declare(null, {
 	}
 });
 
+Store.Collection = Collection;
 Store.PutDirectives = declare(null, {
 	// summary:
 	//		Directives passed to put() and add() handlers for guiding the update and
@@ -177,92 +280,6 @@ Store.QueryOptions = declare(null, {
 	//		The first result to begin iteration on
 	// count: Number?
 	//		The number of how many results should be returned.
-});
-
-Store.QueryResults = declare(null, {
-	// summary:
-	//		This is an object returned from query() calls that provides access to the results
-	//		of a query. Queries may be executed asynchronously.
-
-	forEach: function(callback, thisObject){
-		// summary:
-		//		Iterates over the query results, based on
-		//		https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/forEach.
-		//		Note that this may executed asynchronously. The callback may be called
-		//		after this function returns.
-		// callback:
-		//		Function that is called for each object in the query results
-		// thisObject:
-		//		The object to use as |this| in the callback.
-
-	},
-	filter: function(callback, thisObject){
-		// summary:
-		//		Filters the query results, based on
-		//		https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/filter.
-		//		Note that this may executed asynchronously. The callback may be called
-		//		after this function returns.
-		// callback:
-		//		Function that is called for each object in the query results
-		// thisObject:
-		//		The object to use as |this| in the callback.
-		// returns: dojo/store/api/Store.QueryResults
-	},
-	map: function(callback, thisObject){
-		// summary:
-		//		Maps the query results, based on
-		//		https://developer.mozilla.org/en/Core_JavaScript_1.5_Reference/Objects/Array/map.
-		//		Note that this may executed asynchronously. The callback may be called
-		//		after this function returns.
-		// callback:
-		//		Function that is called for each object in the query results
-		// thisObject:
-		//		The object to use as |this| in the callback.
-		// returns: dojo/store/api/Store.QueryResults
-	},
-	then: function(callback, errorHandler){
-		// summary:
-		//		This registers a callback for when the query is complete, if the query is asynchronous.
-		//		This is an optional method, and may not be present for synchronous queries.
-		// callback:
-		//		This is called when the query is completed successfully, and is passed a single argument
-		//		that is an array representing the query results.
-		// errorHandler:
-		//		This is called if the query failed, and is passed a single argument that is the error
-		//		for the failure.
-	},
-	observe: function(listener, includeAllUpdates){
-		// summary:
-		//		This registers a callback for notification of when data is modified in the query results.
-		//		This is an optional method, and is usually provided by dojo/store/Observable.
-		// listener: Function
-		//		The listener function is called when objects in the query results are modified
-		//		to affect the query result. The listener function is called with the following arguments:
-		//		| listener(object, removedFrom, insertedInto);
-		//
-		//		- The object parameter indicates the object that was create, modified, or deleted.
-		//		- The removedFrom parameter indicates the index in the result array where
-		//		the object used to be. If the value is -1, then the object is an addition to
-		//		this result set (due to a new object being created, or changed such that it
-		//		is a part of the result set).
-		//		- The insertedInto parameter indicates the index in the result array where
-		//		the object should be now. If the value is -1, then the object is a removal
-		//		from this result set (due to an object being deleted, or changed such that it
-		//		is not a part of the result set).
-		// includeAllUpdates:
-		//		This indicates whether or not to include object updates that do not affect
-		//		the inclusion or order of the object in the query results. By default this is false,
-		//		which means that if any object is updated in such a way that it remains
-		//		in the result set and it's position in result sets is not affected, then the listener
-		//		will not be fired.
-
-	},
-	// total: Number|Promise?
-	//		This property should be included in if the query options included the "count"
-	//		property limiting the result set. This property indicates the total number of objects
-	//		matching the query (as if "start" and "count" weren't present). This may be
-	//		a promise if the query is asynchronous.
-	total: 0
 });
 
 Store.Transaction = declare(null, {
