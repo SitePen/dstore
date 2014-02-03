6b3e2a3ecb11d13a38ae922b663483cc11e0f501
Kris Zyp <kriszyp@gmail.com>
2014-02-03 11:29:57 -0700

    Add package information

---
+++ package.js
@@ -0,0 +1,21 @@
+var miniExcludes = {
+		"dstore/README.md": 1,
+		"dstore/package": 1
+	},
+	isTestRe = /\/test\//;
+
+var profile = {
+	resourceTags: {
+		test: function(filename, mid){
+			return isTestRe.test(filename);
+		},
+
+		miniExclude: function(filename, mid){
+			return /\/(?:tests|demos)\//.test(filename) || mid in miniExcludes;
+		},
+
+		amd: function(filename, mid){
+			return /\.js$/.test(filename);
+		}
+	}
+};
