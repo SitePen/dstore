14734084aa59ffa48c6e83e2efc138124b4dbbbe
Kris Zyp <kriszyp@gmail.com>
2014-03-17 07:37:34 -0600

    Remove code to remove leading ? on RQL queries

--- RqlClient.js
+++ RqlClient.js
@@ -10,7 +10,6 @@ define([
 		filter: function (q, options) {
 			// strip the leading '?' since rql doesn't like it
 			if (typeof q === 'string') {
-				q = q.replace(/^\?/, '');
 				// the RQL engine doesn't understand our sort, start, and count properties,
 				// so we apply those constraints after running the RQL query
 				var subCollection = this._createSubCollection({
