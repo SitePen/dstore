64cdba7abea3947315e5ec0c00fd8a7ee22d7258
Brandon Payton <brandon@happycode.net>
2014-04-10 16:51:11 -0700

    Clean up QueryMethod - Change entry  to - Only add a queryer if there is a queryEngine

--- QueryMethod.js
+++ QueryMethod.js
@@ -1,30 +1,24 @@
 define([], function () {
-	// TODO: Remove this comment
-	// * create log
-	// * add queryer based on log
-	// * create new collection with updated log and queryer
-	// * run store-specific query logic
-
 	// TODO: Convert to a single argument w/ queryName as the type
 	return function QueryMethod(queryName, args) {
 		args = args || {};
 
 		return function () {
 			// TODO: Test calling log
-			var logValue = args.log ? args.log.apply(this, arguments) : arguments[0],
+			// TODO: What is a better name than `log`? `createQueryArgument`?
+			var queryArgument = args.log ? args.log.apply(this, arguments) : arguments[0],
 				logEntry = {
 					type: queryName,
-					// TODO: Rename this to `argument`
-					value: logValue,
-					// TODO: Should the default case be null instead?
-					queryer: this.queryEngine
-						? this.queryEngine[queryName](logValue)
-						: function (data) { return data; }
-				},
-				newCollection = this._createSubCollection({
-					queryLog: this.queryLog.concat([ logEntry ]),
-					queryLogTop: logEntry
-				});
+					argument: queryArgument,
+				};
+
+			if (this.queryEngine) {
+				logEntry.queryer = this.queryEngine[queryName](queryArgument);
+			}
+
+			var newCollection = this._createSubCollection({
+				queryLog: this.queryLog.concat([ logEntry ])
+			});
 
 			// TODO: Test calling implementation
 			return args.implementation ? args.implementation(logEntry, newCollection) : newCollection;
