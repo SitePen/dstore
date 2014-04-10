b2eaedaa22c6d8f4dc848dbebfcdce78efb98ae7
Brandon Payton <brandon@happycode.net>
2014-04-10 16:48:39 -0700

    Tweak object literal style in Memory tests

--- tests/Memory.js
+++ tests/Memory.js
@@ -16,11 +16,11 @@ define([
 		beforeEach: function () {
 			store = new Memory({
 				data: [
-					{id: 1, name: 'one', prime: false, mappedTo: 'E'},
-					{id: 2, name: 'two', even: true, prime: true, mappedTo: 'D'},
-					{id: 3, name: 'three', prime: true, mappedTo: 'C'},
-					{id: 4, name: 'four', even: true, prime: false, mappedTo: null},
-					{id: 5, name: 'five', prime: true, mappedTo: 'A'}
+					{ id: 1, name: 'one', prime: false, mappedTo: 'E' },
+					{ id: 2, name: 'two', even: true, prime: true, mappedTo: 'D' },
+					{ id: 3, name: 'three', prime: true, mappedTo: 'C' },
+					{ id: 4, name: 'four', even: true, prime: false, mappedTo: null },
+					{ id: 5, name: 'five', prime: true, mappedTo: 'A' }
 				]
 			});
 
