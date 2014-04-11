5d9aa83b63bc6d0cba561317c473a4bef8afe787
Brandon Payton <brandon@happycode.net>
2014-04-10 23:28:29 -0700

    Add summary comment for RequestMemory

--- RequestMemory.js
+++ RequestMemory.js
@@ -5,6 +5,9 @@ define([
 	'./SimpleQuery'
 ], function(declare, Request, Cache, SimpleQuery) {
 	return declare([ Request, Cache ], {
+		// summary:
+		//		An in-memory store primed by data from an async request
+
 		postscript: function () {
 			this.inherited(arguments);
 			this.fetch();
