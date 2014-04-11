0b7872a5ac9a113b9ce7507c6ecc2cc7f108ef50
Brandon Payton <brandon@happycode.net>
2014-04-10 22:44:19 -0700

    Change StoreAdapter to a wrapper that does not mutate the adapted store

--- legacy/StoreAdapter.js
+++ legacy/StoreAdapter.js
@@ -10,14 +10,21 @@ define([
 
 	var modifyDelegate = function (name) {
 		return function () {
-			var root = this.root;
-			if (root && root !== this) {
-				return root[name].apply(root, arguments);
-			}
-			return this.inherited(arguments);
+			var objectStore = this.objectStore;
+			// TODO: Do we need to serialize models here?
+			return objectStore[name].apply(objectStore, arguments);
 		};
 	};
-	var StoreAdapter = declare(Store, {
+	// TODO: Should this be named ObjectStoreAdapter?
+	return declare(Store, {
+
+		// objectStore:
+		//		The object store wrapped by this adapter
+		objectStore: null,
+
+		constructor: function (kwArgs) {
+			declare.safeMixin(this, kwArgs);
+		},
 
 		get: function () {
 			// summary:
@@ -26,12 +33,9 @@ define([
 			//		The identity to use to lookup the object
 			// returns: Object
 			//		The object in the store that matches the given id.
-			var self = this;
-			var root = this.root;
-			if (root && root !== this) {
-				return this.get.apply(root, arguments);
-			}
-			return when(this.inherited(arguments), function (object) {
+			var self = this,
+				objectStore = this.objectStore;
+			return when(objectStore.get.apply(objectStore, arguments), function (object) {
 				return self._restore(object);
 			});
 		},
@@ -39,6 +43,12 @@ define([
 		put: modifyDelegate('put'),
 		add: modifyDelegate('add'),
 		remove: modifyDelegate('remove'),
+
+		getIdentity: modifyDelegate('getIdentity'),
+		// TODO: Test _setIdentity
+		_setIdentity: function (object, identityArg) {
+			return object[this.objectStore.idProperty] = identityArg;
+		},
 
 		fetch: function () {
 			// summary:
@@ -89,7 +99,7 @@ define([
 			// TODO: It seems strange to just use the first filter without a warning we are discarding the others.
 			//		Maybe we should try to compose multiple filters into a single filter?
 			//		Though it may be an inaccurate composition if more than one filter mentions the same property.
-			var results = (this.root || this).query(filtered[0] || {}, queryOptions);
+			var results = this.objectStore.query(filtered[0] || {}, queryOptions);
 			if (results) {
 				var total = results.total;
 				// apply the object restoration
@@ -97,36 +107,6 @@ define([
 				results.total = total;
 			}
 			return results;
-		},
-		_createSubCollection: function() {
-			var collection = Store.prototype._createSubCollection.apply(this, arguments);
-			collection.root = this.root || this;
-			return collection;
 		}
 	});
-
-	StoreAdapter.adapt = function (obj, config) {
-		// summary:
-		//		Adapts an existing dstore object to behave like a dstore object.
-		// obj: Object
-		//		A dstore object that will have an adapter applied to it.
-		// config: Object?
-		//		An optional configuration object that will be mixed into the adapted object.
-		var adapter = new StoreAdapter();
-		// we need to keep any the original store's own properties
-		for (var i in obj) {
-			if (obj.hasOwnProperty(i)) {
-				adapter[i] = obj[i];
-			}
-		}
-		// we now mixin adapter properties
-		obj = declare.safeMixin(obj, adapter);
-		if (config) {
-			obj = lang.mixin(obj, config);
-		}
-
-		return obj;
-	};
-
-	return StoreAdapter;
 });
