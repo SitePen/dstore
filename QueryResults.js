8df120b337234375f84849ab18806c9756be3f19
Kris Zyp <kriszyp@gmail.com>
2014-06-24 16:34:34 -0600

    Improve the clarity of the totalLength assignment

--- QueryResults.js
+++ QueryResults.js
@@ -11,10 +11,18 @@ define(['dojo/_base/lang', 'dojo/when'], function (lang, when) {
 		var hasTotalLength = options && 'totalLength' in options;
 		if(data.then) {
 			data = lang.delegate(data);
+			// a promise for the eventual realization of the totalLenght, in
+			// case it comes from the resolved data
 			var totalLengthPromise = data.then(function (data) {
-				return (data.totalLength = hasTotalLength ? options.totalLength :
-					data.totalLength || data.length);
+				// calculate total length, now that we have access to the resolved data
+				var totalLength = hasTotalLength ? options.totalLength :
+						data.totalLength || data.length;
+				// make it available on the resolved data
+				data.totalLength = totalLength;
+				return totalLength;
 			});
+			// make the totalLength available on the promise (whether through the options or the enventual
+			// access to the resolved data)
 			data.totalLength = hasTotalLength ? options.totalLength : totalLengthPromise;
 		} else {
 			data.totalLength = hasTotalLength ? options.totalLength : data.length;
