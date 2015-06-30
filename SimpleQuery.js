69b785305934e832110f66d72786410199356059
Bradley Maier <bmaier@sitepen.com>
2015-06-30 14:38:44 -0400

    Make default comparison assert that undefined > null > any other value
    Fixes #131 by making the default comparison function created in
    SimpleQuery#_createSortQuerier treat undefined as greater than any
    value, and null greater than any value besides undefined.
    This conveniently places all null and undefined values at
    the end of a sorted list.
    
    (cherry picked from commit 8453bd112bb3d881434a1b651c09886f4d56dd55)

--- SimpleQuery.js
+++ SimpleQuery.js
@@ -162,10 +162,8 @@ define([
 						return object[properties];
 					});
 			};
-		}
+		},
 
-		/* jshint ignore:start */
-		,
 		_createSortQuerier: function (sorted) {
 			var queryAccessors = this.queryAccessors;
 			return function (data) {
@@ -184,10 +182,16 @@ define([
 
 							aValue != null && (aValue = aValue.valueOf());
 							bValue != null && (bValue = bValue.valueOf());
-
-							comparison = aValue === bValue
-								? 0
-								: (!!descending === (aValue === null || aValue > bValue && bValue !== null) ? -1 : 1);
+							if (aValue === bValue) {
+								comparison = 0;
+							}
+							else {
+								// Prioritize undefined > null > defined
+								var isALessThanB = typeof bValue === 'undefined' ||
+									bValue === null && typeof aValue !== 'undefined' ||
+									aValue != null && aValue < bValue;
+								comparison = Boolean(descending) === isALessThanB ? 1 : -1;
+							}
 						}
 
 						if (comparison !== 0) {
@@ -199,6 +203,5 @@ define([
 				return data;
 			};
 		}
-		/* jshint ignore:end */
 	});
 });
